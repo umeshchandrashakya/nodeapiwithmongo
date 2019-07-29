7f5fb20d592c5a498cd5f430ba1695cf884348c3
umeshchandrashakya <ucs9621@gmail.com>
2019-07-29 23:58:05 +0530

    fist

---
+++ routes/subscribers.js
@@ -0,0 +1,38 @@
+const experess = require("express");
+const router = experess.Router();
+const subscriber = require("../models/subscriber");
+
+//get All subscriber
+
+router.get("/", async (req, res, next) => {
+  try {
+    const subscribes = await subscriber.find();
+    res.json(subscribes);
+  } catch (error) {
+    res.status(5000).json({ message: error.message });
+  }
+});
+
+//get by id
+router.get("/:id", (req, res, next) => {});
+
+router.post("/", async (req, res, next) => {
+  const newsubscriber = new subscriber({
+    name: req.body.name,
+    subscribeToChannel: req.body.subscribeToChannel
+    //subscribeDate: req.body.subscribeDate
+  });
+  try {
+    const subscribe = await newsubscriber.save();
+    res.status(201).json(subscribe);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+//update
+router.patch("/", (req, res, next) => {});
+
+router.delete("/:id", (req, res, next) => {});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATA_BASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", error => console.error(error));
+db.once("open", () => console.log("connected to database"));
+app.use(express.json());
+
+const subscribers1 = require("./routes/subscribers");
+app.use("/subscribers", subscribers1);
+
+app.listen(3000, () => console.log("server started"));
